e34fbe9b5308eafbe9b18493e862d7eb504baf0f
vinirv <vinirv@gmail.com>
2020-06-17 23:44:48 -0400

    Commit inicial

---
+++ NiboWebApp/NiboWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NiboWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
